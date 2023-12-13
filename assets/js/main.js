582c0ba7ed0c6f6e7f417e0f9048d244be236f3b
renehellmich <renehellmich94@gmail.com>
2023-12-13 14:43:47 +0100

    javascript code completed

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+function check() {
+    let size = document.querySelector("#bodyheight").value
+    let age = document.querySelector("#age").value
+    let output = document.querySelector(".output")
+    const statue = document.getElementsByName('auswahl');
+    let selectedValue = null;
+    for (const radio of statue) {
+        if (radio.checked) {
+            selectedValue = radio.value;
+            console.log(selectedValue)
+            break;
+        }
+    }
+    switch (selectedValue) {
+        case "schmal":
+            output.innerHTML = [(Number(size) - 100) + (Number(age) / 10)0.9 + 0.9]
+            console.log(output.innerHTML = [(Number(size) - 100) + (Number(age) / 10)0.9 + 0.9])
+            break
+        case "breit":
+            output.innerHTML = [(Number(size) - 100) + (Number(age) / 10) * 0.9 + 1.1]
+            break
+    }
+}
